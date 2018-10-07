c89028b89032deea3a7f9efeedd7108d920c26ce
yegresvonason <yegresvonason@gmail.com>
2018-10-07 23:19:54 +0300

    autoprefixer

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,14 +2,24 @@
 
 var gulp = require('gulp'),
 	 sass = require('gulp-sass'),
+	 autoprefixer = require('gulp-autoprefixer'),
 	 htmlmin = require('gulp-htmlmin');
 
 gulp.task('sass', function () {
 	return gulp.src('./dev/sass/**/*.scss')
 				.pipe(sass().on('error', sass.logError))
 				// .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-				.pipe(gulp.dest('./build/css'));
+				.pipe(gulp.dest('./dev/css/'));
 });
+
+gulp.task('autoprefixer', () => 
+	gulp.src('./dev/css/style.css')
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('./build/css/'))
+);
 
 gulp.task('minify', () => {
 	return gulp.src('dev/*.html')
@@ -18,7 +28,7 @@ gulp.task('minify', () => {
 });
 
 gulp.task('watch', function () {
-	gulp.watch('./dev/sass/**/*.scss', ['sass']);
+	gulp.watch('./dev/sass/**/*.scss', ['sass', 'autoprefixer']);
 	gulp.watch('./dev/*.html', ['minify']);
 });
-gulp.task('default', ['sass', 'minify', 'watch']);
+gulp.task('default', ['sass', 'autoprefixer', 'minify', 'watch']);
